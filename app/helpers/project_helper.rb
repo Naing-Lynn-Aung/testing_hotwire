3aa119be12dd62380ed3f59e1b59df80c5aed413
Naing-Lynn-Aung <nainglynnaung25799@gmail.com>
2025-07-08 20:54:35 +0700

    add search function

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,2 +1,5 @@
 module ProjectHelper
+  def highlight_query
+    params[:search].to_s.split(" ")
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,8 @@ class Project < ApplicationRecord
 
   validates :content, presence: true
   has_many :tasks, dependent: :destroy
+
+  def self.search(search)
+    where("title LIKE ? OR content LIKE ?", "%#{search.strip}%", "%#{search.strip}%")
+  end
 end
